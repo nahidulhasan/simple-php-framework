d9ded061311599c7bf64c78a4fbcbd899aa7591c
nahid35 <nahidul.cse@gmail.com>
2018-07-06 23:07:58 +0600

    Added index file

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = isset($_GET['name']) ? $_GET['name'] : 'World';
+
+header('Content-Type: text/html; charset=utf-8');
+
+printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
