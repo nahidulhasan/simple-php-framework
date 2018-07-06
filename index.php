3130597843b0fe3baca6d5b793dc2e2452064c4a
nahid35 <nahidul.cse@gmail.com>
2018-07-06 23:12:07 +0600

    Modify index file

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-$name = isset($_GET['name']) ? $_GET['name'] : 'World';
+require_once __DIR__.'/vendor/autoload.php';
 
-header('Content-Type: text/html; charset=utf-8');
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+$request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
