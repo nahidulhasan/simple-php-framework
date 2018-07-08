c692b0ca634b5cf8e3b09fa769b3576346c72c95
Nahidul Hasan <nahidul.cse@gmail.com>
2018-07-08 18:46:15 +0600

    Refactored code

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,4 +1,1 @@
-<?php
-<?php $name = $request->get('name', 'World') ?>
-
-Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+Hello <?php echo htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
