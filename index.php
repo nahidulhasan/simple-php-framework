558f9a768bd2c7deff08d0fbcd799c971454f6aa
Nahidul Hasan <nahidul@pathao.com>
2018-07-08 18:19:35 +0600

    Modify index file

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
-
-require_once __DIR__.'/vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
+require_once __DIR__.'/init.php';
 
 $name = $request->get('name', 'World');
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
-
+$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
 $response->send();
