b8345919d52b4028a85eb377e74d5b8cc4954e54
Nahidul Hasan <nahidul.cse@gmail.com>
2018-07-23 18:05:20 +0600

    Retrieve data from twitter api

--- src/API/Controller/APIController.php
+++ src/API/Controller/APIController.php
@@ -3,12 +3,37 @@ namespace API\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use TwitterAPIExchange;
 
 
 class APIController
 {
     public function index(Request $request, $str)
     {
-        var_dump($str); exit;
+       // var_dump($str); exit;
+
+        $settings = array(
+            'oauth_access_token' => "164222452-hlvpCDNQgtZqjGszAdTnohnzq2Enzf1JiCW5sLlq",
+            'oauth_access_token_secret' => "25odCoYSf2D2DKFjYqaCLrO33Hj01chx7FASOPx8uXLgN",
+            'consumer_key' => "AqPfq62fwRqxhlv1OiKfv374D",
+            'consumer_secret' => "xA8oMWH3twGefixJISjhrcUdLXNJwlmteMdXVnX2NmGgb7e1K7"
+        );
+
+
+        $url = 'https://api.twitter.com/1.1/search/tweets.json';
+        $requestMethod = 'GET';
+
+        $getfield = "?q=#laravel";
+
+        $twitter = new TwitterAPIExchange($settings);
+
+
+        $response =  $twitter->setGetfield($getfield)
+            ->buildOauth($url, $requestMethod)
+            ->performRequest();
+
+        var_dump($response);
+
+
     }
 }
